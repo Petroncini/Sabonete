f7621b7acc1fd842b5a0172505f6fa063e32bb24
Petroncini <caiopetroncini@gmail.com>
2026-06-22 11:54:52 -0300

    idk

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,6 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '../')));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
